99b9eb718a2888e1fd472e70cfc1203cd493c2d1
JM Salceod <jmsalcedo2016@gmail.com>
2018-01-27 21:22:21 -0800

    PHP DB CONNECTION AUTO CREATE TABLES

---
+++ admin-dashboard.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Quezon City- Stats</title>
+
+<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
+<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+ 
+ </head>
+ <body>
+
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">HEAD GOVERMENT</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">BARANGGAY ISSUE</a></li>
+      <li class="active"><a href="#">SETTINGS</a></li>
+       </ul>
+  </div>
+</nav>
+  
+<div class="container">
+<div class="row">
+  <form class="form-inline">
+     <center>
+    <label>BARANGAY: </label>
+    <input type="text" class="form-control" name="brgy" placeholder="Enter Baranggay Here...">
+    <button class="btn btn-primary">ADD</button><br><br>
+  </center>
+  </form>
+</div>        
+ <div class="row">
+ <div class="col-md-9">
+
+   <table class="table table-hover" border="1">
+    <thead>
+      <tr>
+        <th>BARANGGAY</th>
+        <th>ACTIONS</th>
+       
+      </tr>
+    </thead>
+    
+    <tbody>
+      <tr>
+        <td>BRGY PAGASA</td>
+        <td><a href="#">EDIT </a> <a href="#">VIEW</a>  <a href="#">DELETE</a></td>
+        
+      </tr>
+      <tr>
+        <td>BRGY BAHAY TORO</td>
+        <td><a href="#">EDIT </a> <a href="#">VIEW</a>  <a href="#">DELETE</td> 
+      </tr>
+      
+    </tbody>
+  
+  </table>
+ </div> 
+ <div class="col-md-3">
+  <table class="table table-bordered" border="1">
+    <thead>
+      <tr>
+        <th>ISSUES</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>dasdsadsads</td>
+      </tr>
+      <tr>
+        <td>dasdsadsads</td>
+      </tr>
+      <tr>
+        <td>dasdsadsads</td>
+      </tr>
+      <tr>
+        <td>dasdsadsads</td>
+      </tr>
+
+      <tr>
+        <center>
+        <td><input type="text" name="txt1" style="width: 100%" placeholder="Enter Text Here..."></td>
+        </center>       
+      </tr>
+
+      <tr>
+        <center>
+        <td><button type="button" class="btn btn-primary" style="width:100%">ADD</button></td>
+          </center>
+      </tr>
+    </tbody>
+  </table>
+ </div>
+
+
+
+
+ </div>
+
+
+</div>
+
+</body>
+</html>
---
+++ dbconnection.php
@@ -0,0 +1,72 @@
+<?php
+	$server="localhost";
+	$username="root";
+	$password="";
+	$conn=new mysqli($server, $username, $password);
+	$database="nation_building";
+	$result=$conn->query("SHOW DATABASES");
+	$db_exist=false;
+	while($row=$result->fetch_assoc())
+	{
+		if($row['Database']===$database)
+		{
+			$db_exist=true;
+			break;
+		}
+	}
+	if(!$db_exist)
+	{
+		$conn->query("CREATE DATABASE ".$database);
+		$conn=new mysqli($server, $username, $password, $database);
+	}
+	else
+	{
+
+		$conn=new mysqli($server, $username, $password, $database);
+	}
+	$result=$conn->query("SHOW TABLES FROM ". $database);
+	$users=$barangays=$issues=$barangay_issue=false;
+	while($row=$result->fetch_assoc())
+	{
+		if($row['Tables_in_nation_building']==="users") $users=true;
+		if($row['Tables_in_nation_building']==="barangays") $barangays=true;
+		if($row['Tables_in_nation_building']==="issues") $issues=true;
+		if($row['Tables_in_nation_building']==="barangay_issue") $barangay_issue=true;
+	}
+	if(!$users)
+	{
+		$password=md5("admin");
+		$query="CREATE table users(
+		id INT(3) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+		username VARCHAR(30) NOT NULL,
+		password VARCHAR(30) NOT NULL,
+		type VARCHAR(30) NOT NULL)";
+		$conn->query($query);
+		$sql="INSERT INTO users(username, password, type) VALUES('admin', '$password', 'admin')";
+		$conn->query($sql);
+		$conn->error;
+	}
+	if(!$barangays)
+	{
+		$query="CREATE table barangays(
+		id INT(3) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+		username VARCHAR(30) NOT NULL)";
+		$conn->query($query);
+	}
+	if(!$issues)
+	{
+		$query="CREATE table issues(
+		id INT(3) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+		issue_name VARCHAR(30) NOT NULL)";
+		$conn->query($query);
+	}
+	if(!$barangay_issue)
+	{
+		$query="CREATE table barangay_issue(
+		id INT(3) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+		issue INT(3) NOT NULL,
+		barangay INT(3) NOT NULL,
+		status VARCHAR(30) NOT NULL)";
+		$conn->query($query);
+	}
+?>
